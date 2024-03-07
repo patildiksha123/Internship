c8739b0cdd1941a4610fec4c9463dd2c76cea94d
patildiksha123 <159021773+patildiksha123@users.noreply.github.com>
2024-03-07 15:49:46 +0530

    first commit

--- diksha/home/views.py
+++ diksha/home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return  render (request , "index.html.html")
+    return  render (request , "index.html")
 
 def signup(request):
     return  render (request , "signup.html")
